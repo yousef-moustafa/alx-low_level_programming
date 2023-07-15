501fe64851654dbaeb58982950b7807a44081f24
Yousef Moustafa <ymoustafa005@gmail.com>
2023-07-14 20:01:24 -0700

    idk

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digit numbers followed by comma space
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	char num = 48;
+
+	while (num <= 56)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar(57);
+}

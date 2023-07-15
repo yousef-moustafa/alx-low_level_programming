b685c48f669e69ca60eae0067a3cbf16feb36307
Yousef Moustafa <ymoustafa005@gmail.com>
2023-07-14 19:28:05 -0700

    lower then upper alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lower then upper followed by new line
+ *
+ * return: always 0  (Success)
+*/
+
+int main(void)
+{
+	char *lower_alph = "abcdefghijklmnopqrstuvwxyz"
+	char *upper_alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for (int i = 0; i <26; i++)
+	{
+		putchar(lower_alph[i])
+	}
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(upper_alph[i])
+	}
+	putchar("\n")
+	return (0);
+}

13c31f327ba7aeb844c29a0fe483069a3de52563
Yousef Moustafa <ymoustafa005@gmail.com>
2023-07-19 02:21:40 -0700

    confidence is key

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: number input
+ *
+ * Return: value of last digit
+*/
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	n = n % 10;
+	return (n);
+}

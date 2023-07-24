3b475ad527977a6818e54f11c224cb972048770a
Yousef Moustafa <ymoustafa005@gmail.com>
2023-07-24 01:00:47 -0700

    first try

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char + newline
+ * 
+ * @str: first char of string
+ *
+ * Return: void
+*/
+
+void puts2(char *str)
+{
+	int num;
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	for (num = 0; num <= length; num++)
+	{
+		if (num % 2 != 0)
+		{
+			continue;
+		}
+		_putchar(str[num]);
+	}
+	_putchar('\n');
+}

87e47b40fe8aff9f3c2fd567f79d27532202247f
Yousef Moustafa <ymoustafa005@gmail.com>
2023-07-23 23:47:41 -0700

    sixth

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: first char
+ *
+ * Return: void
+*/
+
+void rev_string(char *s)
+{
+	char *tmp = NULL;
+	char *end = s - 1;
+
+	while (s != '\0')
+	{
+		s++;
+	}
+
+	s--;
+
+	while (s != end)
+	{
+		*tmp = *s;
+		s--;
+	}
+
+	s = tmp;
+}

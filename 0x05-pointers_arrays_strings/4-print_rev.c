154cf9e6b3e13c8c75c6c4eff56bf811f280d597
Yousef Moustafa <ymoustafa005@gmail.com>
2023-07-23 23:22:51 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse order
+ *
+ * @s: first char
+ *
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int end = s - 1;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+
+	while (s != end)
+	{
+		_putchar(*s);
+		s--;
+	}
+}
